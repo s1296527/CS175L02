bb1a2d4fd606a67406700082fa851dddb20b6489
s1296527 <s1296527@cssemaclin11.monmouth.edu>
2020-02-03 14:56:33 -0500

    house painting algorithm

--- HelloWorld/src/hello.java
+++ HelloWorld/src/hello.java
@@ -1,10 +1,61 @@
-
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!!!");
+		
+		System.out.print("This ");
+		System.out.print("is ");
+		System.out.print("my ");
+		System.out.print("first ");
+		System.out.println("program");
+		
+		System.out.println("3 + 4");
+		
+		System.out.println(3 + 4);
 
+		System.out.println("x =" + 10);
+		
+		System.out.println("x = + 10");
+		
+		//This line has a error: System.out.println(1/0);
+		
+		/*
+		these
+		
+		lines 
+		are 
+		
+		errors 
+		*/
+		
+		int width = 20;
+		
+		System.out.println("width = " + width);
+		
+		String myName = "Edward";
+		
+		System.out.println("My name is " + myName);
+		
+		width = 35;
+		
+		myName = "BIG ED MOE";
+		
+		System.out.println("width = " + width);
+		
+		System.out.println("My name is " + myName);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
